90a474355e3cf8333768de03e440252a55565a4a
KJES4 <kathrynjeanelizabethstacy@gmail.com>
2024-05-07 12:32:38 -0500

    Add dark theme toggle

---
+++ starter_app/src/styles/palette.ts
@@ -0,0 +1,147 @@
+import { PaletteOptions } from "@mui/material";
+
+export const lightModePalette: PaletteOptions = {
+  primary: {
+    main: "#3952CD",
+    container: "#DEE0FF",
+    onContainer: "#00115A",
+    fixed: "#DEE0FF",
+    fixedDim: "#BAC3FF",
+    onFixed: "#00115A",
+    onFixedVariant: "#1937B4",
+  },
+  secondary: {
+    main: "#B32A00", 
+    container: "#FFDBD2",
+    onContainer: "#3C0800",
+    fixed: "#FFDBD2",
+    fixedDim: "#FFB4A2",
+    onFixed: "#3C0800",
+    onFixedVariant: "#891E00",
+  },
+  tertiary: {
+    main: "#6B5F00",
+    container: "#F5E47F",
+    onContainer: "#201C00",
+    fixed: "#F5E47F",
+    fixedDim: "#D8C866",
+    onFixed: "#201C00",
+    onFixedVariant: "#504700",
+  },
+  error: { 
+    main: "#BA1A1A", 
+    container: "#FFDAD5",
+    onContainer: "#410002",
+  },
+  surfaceDim: { 
+    main: "#DAD9E3", 
+    },
+  surface: { 
+    main: "#FDF8F8", 
+    },
+  surfaceBright: { 
+    main: "#FDF8F8", 
+    },
+  containerLowest: { 
+    main: "#FFF", 
+    },
+  containerLow: { 
+    main: "#F4F2FD", 
+    },
+  container: { 
+    main: "#EEEDF7", 
+    },
+  containerHigh: { 
+    main: "#E8E7F2", 
+    },
+  containerHighest: { 
+    main: "#E5E2E1", 
+    },
+  onSurface: { 
+    main: "#1C1B1B", 
+    },
+  onVariant: { 
+    main: "#474647", 
+    },
+  outline: { 
+    main: "#787677", 
+    },
+  outlineVariant: { 
+  main: "#C9C6C6",
+  },
+ };
+ 
+ export const darkModePalette: PaletteOptions = {
+  primary: {
+    main: "#BAC3FF",
+    onMain: "#001F8F",
+    container: "#1A37B5",
+    onContainer: "#DEE0FF",
+    fixed: "#DEE0FF",
+    fixedDim: "#BAC3FF",
+    onFixed: "#00105B",
+    onFixedVariant: "#1A37B5",
+  },
+  secondary: {
+    main: "#FFB5A0", 
+    onMain: "#5F1500",
+    container: "#862200",
+    onContainer: "#FFDBD1",
+    fixed: "#FFDBD1",
+    fixedDim: "#FFB5A0",
+    onFixed: "#3B0A00",
+    onFixedVariant: "#862200",
+  },
+  tertiary: {
+    main: "#DBC84E",
+    onMain: "#373100",
+    container: "#504700",
+    onContainer: "#F8E467",
+    fixed: "#504700",
+    fixedDim: "#DBC84E",
+    onFixed: "#F8E467",
+    onFixedVariant: "#504700",
+  },
+  error: { 
+    main: "#FFB4AB", 
+    onMain: "#690005",
+    container: "#93000A",
+    onContainer: "#FFDAD6",
+  },
+  surfaceDim: { 
+    main: "#12131A", 
+    },
+  surface: { 
+    main: "#12131A", 
+    },
+  surfaceBright: { 
+    main: "#383941", 
+    },
+  containerLowest: { 
+    main: "#0D0E15", 
+    },
+  containerLow: { 
+    main: "#1A1B23", 
+    },
+  container: { 
+    main: "#1E1F27", 
+    },
+  containerHigh: { 
+    main: "#292931", 
+    },
+  containerHighest: { 
+    main: "#33343C", 
+    },
+  onSurface: { 
+    main: "#E3E1EC", 
+    },
+  onVariant: { 
+    main: "#C5C5D6", 
+    },
+  outline: { 
+    main: "#8F909F", 
+    },
+  outlineVariant: { 
+  main: "#444654",
+  },
+ };
