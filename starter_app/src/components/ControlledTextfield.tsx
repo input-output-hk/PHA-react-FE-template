aa728fb20019260a3e8e85b04f5ca2bee3953545
KJES4 <kathrynjeanelizabethstacy@gmail.com>
2025-11-21 13:57:22 -0600

    add drawer component

--- starter_app/src/components/ControlledTextfield.tsx
+++ starter_app/src/components/ControlledTextfield.tsx
@@ -97,10 +97,10 @@ export default function ControlledTextField({
             className='w-full border-none bg-transparent block outline-none text-sm text-onSurface pr-0 pl-[14px] pb-[8.5px] pt-[8.5px] placeholder:text-onSurface/50'
           />
           <div className="flex align-middle">
-            <Button 
+            <Button
               variant="embedded"
-              onMouseDown={handleClear} 
-              content={hasError && !isFocused ? {svg: <ExclamationCircleIcon />, mode: 'stroke', strokeWidth: 1.5, color: 'error'} : { svg: <XCircleIcon />, mode: 'stroke', strokeWidth: 1.5, color: 'onVariant' }} />
+              onMouseDown={handleClear}
+              content={hasError && !isFocused ? { svg: <ExclamationCircleIcon />, mode: 'stroke', size: 'small', strokeWidth: 1.5, color: 'error' } : {svg: <XCircleIcon />, mode: 'stroke', size: 'small', strokeWidth: 1.5, color: 'onVariant'}}/>
           </div>
         </div>
         <p className={cn('text-xs mt-1', hasError ? 'text-error' : 'text-onSurface/60')}>
